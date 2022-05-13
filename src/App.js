3ae425699c3b967467c0ac7e93594a34ab953707
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-05-13 23:09:12 +0700

    add git hub

---
+++ src/App.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  createTable,
+  getDBConnection,
+  getUsers,
+  updateUser,
+} from './modules/sqllite.module';
+import AddUser from './screen/adduser';
+import UserList from './screen/users';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [savedUsers, setsavedUsers] = useState([]);
+  const loadInitialData = useCallback(async () => {
+    try {
+      const db = await getDBConnection();
+      await createTable(db);
+      const initData = await getUsers(db);
+      if (initData.length > 0) {
+        setsavedUsers(initData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const onUpdate = (id, changes) => {
+    console.log(id, changes);
+    const data = savedUsers;
+    const index = data.findIndex(d => d.id === id);
+    data[index] = changes;
+
+    updateUser(data[index]);
+  };
+  useEffect(() => {
+    loadInitialData();
+  }, [loadInitialData]);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Users">
+          {props => (
+            <UserList
+              initData={savedUsers}
+              {...props}
+              load={loadInitialData}
+              deleteu={id => setsavedUsers(savedUsers.filter(u => u.id != id))}
+              updateu={data => onUpdate(data.id, data)}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Add">
+          {props => (
+            <AddUser
+              {...props}
+              add={user => setsavedUsers(savedUsers.concat(user))}
+            />
+          )}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
